099ba2afa781d366c2aa0bf8d3c6fe7de48e127f
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-08-15 19:06:36 -0300

    feat: create first form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import { useState } from 'react';
-
-import MostrarNome from './components/Nome';
+import { useState } from "react";
 
 function App() {
-  const [nome, setNome] = useState('Rafael Costa');
+  const [usuario, setUsuario] = useState('');
+  const [idade, setIdade] = useState('');
+  const [email, setEmail] = useState('');
 
-  function handleChangeName (nome) 
+  const [cliente, setCliente] = useState({});
+
+  function handleRegister (e)
   {
-    setNome(nome);
+    e.preventDefault();
+
+    setCliente({
+      nome: usuario,
+      email: email,
+      idade: idade,
+    })
+
+    alert('Cliente registrado com sucesso!')
   }
 
   return ( // Aqui dentro vai o JSX ou TSX
     <div>
-      <h1>Bem vindo ao meu primeiro App!</h1>
-      <h2>Olá: {nome}</h2>
+      <h2>Cadastro de Usuário</h2>
 
-      <button onClick={ () => handleChangeName('Eu') }>Mudar Nome</button>
+      <form onSubmit={ handleRegister }>
+        <label>Nome</label> <br/>
+        <input 
+          placeholder="Digite seu nome" 
+          value={usuario}
+          onChange={ e => setUsuario(e.target.value)}
+        />
+        <br/><br/>
+
+        <label>Email</label> <br/>
+        <input 
+          placeholder="Digite seu email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <br/><br/>
+
+        <label>Idade</label> <br/>
+        <input 
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={e => setIdade(e.target.value)}
+        />
+        <br/><br/>
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+        <span>Bem vindo.: {cliente.nome}</span> <br/>
+        <span>Idade..........: {cliente.idade}</span> <br/>
+        <span>E-mail........: {cliente.email}</span> <br/>
+      </div>
     </div>
   );
 }
