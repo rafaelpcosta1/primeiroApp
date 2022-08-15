95de5b0f2bc6ae78efb8f4cc6049cd2e07716c74
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-08-15 17:18:01 -0300

    feat: using useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import MostrarNome from "./components/Nome";
+import { useState } from 'react';
+
+import MostrarNome from './components/Nome';
 
 function App() {
+  const [nome, setNome] = useState('Rafael Costa');
+
+  function handleChangeName (nome) 
+  {
+    setNome(nome);
+  }
+
   return ( // Aqui dentro vai o JSX ou TSX
     <div>
       <h1>Bem vindo ao meu primeiro App!</h1>
-      <MostrarNome nome="Leila Mônica" idade="28" />
-      <br />
-      <MostrarNome nome="Rafael" idade="30"/>
+      <h2>Olá: {nome}</h2>
+
+      <button onClick={ () => handleChangeName('Eu') }>Mudar Nome</button>
     </div>
   );
 }
